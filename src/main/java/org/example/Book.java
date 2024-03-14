f52baf5b2bf83fd17ebc5dc7bddce5b132e74d9d
devdomsos <devdomsos@gmail.com>
2024-03-14 11:29:18 +0100

    First commit

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Book {
+    String title;
+    String isbn;
+
+    String author;
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
