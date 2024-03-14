f5bfe805f9ed1b2c9b4f61198ae06ac09d21061e
BaselQq <73541125+BaselQq@users.noreply.github.com>
2024-03-14 12:53:33 +0100

    enhance library

--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -18,20 +18,6 @@ public class Library {
     }
 
     public Book[] getBooks() {
-        Book book1 = new Book();
-        book1.setAuthor("book1");
-        book1.setIsbn(123);
-
-        Book book2 = new Book();
-        book2.setAuthor("book2");
-        book2.setIsbn(321);
-
-        // TODO: Look for better way to add objects in one array
-        Book[] books = new Book[2];
-
-        books[0] = book1;
-        books[1] = book2;
-
         return books;
     }
 
@@ -54,5 +40,9 @@ public class Library {
                 ", LibraryId=" + LibraryId +
                 ", books=" + Arrays.toString(books) +
                 '}';
+    }
+
+    public void removeBooks() {
+        this.books = null;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,11 +8,34 @@ public class Main {
 //        System.out.println(newBook);
 
         Library library1 = new Library();
+        library1.setLibraryName("library1");
+        library1.setLibraryId(1);
         Book[] books = library1.getBooks();
 
-        for (Book book : books) {
-            System.out.println("All Books are" + book);
+        System.out.println(library1);
+
+        Book book1 = new Book();
+        book1.setAuthor("book1");
+        book1.setIsbn(123);
+
+        Book book2 = new Book();
+        book2.setAuthor("book2");
+        book2.setIsbn(321);
+
+        // TODO: Look for better way to add objects in one array
+        Book[] booksAsArray = new Book[2];
+
+        booksAsArray[0] = book1;
+        booksAsArray[1] = book2;
+
+        library1.setBooks(booksAsArray);
+
+        for (Book book : library1.getBooks()) {
+            System.out.println("Book" + book);
         }
 
+        System.out.println("Remove all books from library");
+        library1.removeBooks();
+        System.out.println(library1.getBooks());
     }
 }
