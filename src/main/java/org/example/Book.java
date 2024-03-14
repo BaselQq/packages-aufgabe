a5a8a472e9395f4160342da9fc3c60c1923e240b
BaselQq <73541125+BaselQq@users.noreply.github.com>
2024-03-14 11:28:52 +0100

    create Book class and getter/setter

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,41 @@
+package org.example;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private int isbn;
+    private int pages;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+}
